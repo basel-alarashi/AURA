f86d54ac89e2ce988ece51793f29a80bfb70d9f6
Basel <www.basel10001@gmail.com>
2024-08-25 05:10:06 +0300

    preparing app routes & structure.

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="font-pblack text-3xl">Aora!</Text>
+    </View>
+  );
+}
