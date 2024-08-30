212ef8fbda67fa63916a5a0f545e380d215b1438
Basel <www.basel10001@gmail.com>
2024-08-31 02:57:04 +0300

    sign up page.

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,38 @@
-import { Text, View } from 'react-native';
-import React from 'react';
+import { Image, Text, View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import FormFiled from '@/components/FormFiled';
+import CustomButton from '@/components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignUp = () => {
+  const [form, setForm] = useState({ email: '', password: '', username: '' });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const submitForm = () => { };
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView>
+        <View className='w-full min-h-[93vh] justify-center px-4 my-6'>
+          <Image source={images.logo} resizeMode='contain' className='w-[115px] h-[35px]' />
+          <Text className='text-2xl text-white mt-10 font-psemibold'>Sign up to Aora</Text>
+          <FormFiled title='User Name' value={form.username} otherStyle='mt-7'
+            handleChange={(e: any) => setForm({ ...form, username: e })} placeholder='User Name' />
+          <FormFiled title='Email' value={form.email} otherStyle='mt-7' keyboardType='email-address'
+            handleChange={(e: any) => setForm({ ...form, email: e })} placeholder='Email' />
+          <FormFiled title='Password' value={form.password} otherStyle='mt-7'
+            handleChange={(e: any) => setForm({ ...form, password: e })} placeholder='Password' />
+          <CustomButton title='Sign Up' handlePress={submitForm}
+            containerStyle='mt-7' isLoading={isSubmitting} />
+          <View className='justify-center flex-row gap-2 pt-5'>
+            <Text className='text-lg text-gray-100 font-pregular'>Already have an account?</Text>
+            <Link href={'/(auth)/sign-in'} className='text-lg text-secondary font-psemibold'>Sign In</Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
