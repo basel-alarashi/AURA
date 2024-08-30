94e9facb77de492d978854ec538b50b2452ce61a
Basel <www.basel10001@gmail.com>
2024-08-31 02:55:51 +0300

    the main layout of auth screens.

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,15 @@
-import { Text, View } from 'react-native';
 import React from 'react';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function AuthLayout() {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <React.Fragment>
+      <Stack>
+        <Stack.Screen name='sign-in' options={{ headerShown: false }} />
+        <Stack.Screen name='sign-up' options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style='light' backgroundColor='#FFA001' />
+    </React.Fragment>
   );
 }
