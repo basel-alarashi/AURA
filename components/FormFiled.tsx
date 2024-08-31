31e757f496ab2018a9396d3f11e24da6bcfa990e
Basel <www.basel10001@gmail.com>
2024-08-31 23:37:36 +0300

    correct syntax.

--- components/FormFiled.tsx
+++ components/FormFiled.tsx
@@ -16,7 +16,7 @@ const FormFiled = ({ title, value, handleChange, otherStyle, keyboardType, place
                     onChangeText={handleChange} secureTextEntry={title === 'Password' && !showPassword}
                     className='w-full h-full px-4 bg-black-100 border-black-200 border-2 rounded-2xl items-center caret-secondary text-base font-psemibold text-white focus:border-secondary' />
                 {title === 'Password' &&
-                    <TouchableOpacity className='absolute -top-[-30%] -right-0 mr-2'
+                    <TouchableOpacity className='absolute top-[30%] right-2'
                         onPress={() => setshowPassword(!showPassword)}>
                         <Image source={showPassword ? icons.eyeHide : icons.eye}
                             resizeMode='contain' className='w-6 h-6' />
