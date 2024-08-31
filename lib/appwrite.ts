c2b7438e18f2e0dc4bdfc88370be21d767d29ecd
Basel <www.basel10001@gmail.com>
2024-09-01 02:36:14 +0300

    get all posts function & create storage bucket for thumbnails.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -7,7 +7,8 @@ export const config = {
     databaseId: '66d25e0a003dffbaa9a1',
     userCollectionId: '66d25e4a003bd15119f5',
     videoCollectionId: '66d25e96003c542d7e59',
-    storageId: '66d261d3003c2e830ca9'
+    thumbsStorageId: '66d39369001ea8932c23',
+    videosStorageId: '66d261d3003c2e830ca9'
 };
 
 const client = new Client();
@@ -67,6 +68,17 @@ export const getCurrentUser = async () => {
         if (!currentUser) throw Error('Error in fetching user information.');
 
         return currentUser.documents[0];
+    } catch (error: any) {
+        console.log(error);
+        throw new Error(error);
+    }
+};
+
+export const getAllPosts = async () => {
+    try {
+        const posts = await databases.listDocuments(config.databaseId, config.videoCollectionId);
+
+        return posts.documents;
     } catch (error: any) {
         console.log(error);
         throw new Error(error);
