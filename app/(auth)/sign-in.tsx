395dedc750d8495bbfb13bbc019bac01adf27df0
Basel <www.basel10001@gmail.com>
2024-08-31 21:11:12 +0300

    sign in action.

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,16 +1,33 @@
-import { Image, Text, View, ScrollView } from 'react-native';
+import { Image, Text, View, ScrollView, Alert } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../../constants';
 import FormFiled from '@/components/FormFiled';
 import CustomButton from '@/components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { signIn } from '../../lib/appwrite';
 
 const SignIn = () => {
   const [form, setForm] = useState({ email: '', password: '' });
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submitForm = () => {};
+  const submitForm = async () => {
+    if (!form.email || form.password) {
+      Alert.alert('Error', 'Please fill in all the fileds.');
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      // set it to global state
+      router.replace('/home');
+    } catch (error: any) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className='bg-primary h-full'>
