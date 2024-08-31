651ce517e91d79bdb995dc0af1e4728922678455
Basel <www.basel10001@gmail.com>
2024-09-01 01:01:03 +0300

    Home page.

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import SearchInput from '@/components/SearchInput';
+import Trending from '@/components/Trending';
+import EmptyState from '@/components/EmptyState';
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // fetch videos list.
+    setRefreshing(false);
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <FlatList data={[
+        // { $id: 1 }, { $id: 2 }, { $id: 3 },{ $id: 4 },{ $id: 5 },{ $id: 6 }
+      ]} keyExtractor={(item: any) => item.$id} renderItem={({ item }: any) => (
+        <Text className='text-3xl text-white'>{item.$id}</Text>
+      )} ListHeaderComponent={() => (
+        <View className='my-6 px-4 space-y-6'>
+          <View className='mb-6 flex-row items-start justify-between'>
+            <View>
+              <Text className='text-sm text-gray-100 font-pmedium'>Welcome Back</Text>
+              <Text className='text-2xl text-white font-psemibold'>Basel</Text>
+            </View>
+            <View className='mt-1.5'>
+              <Image source={images.logoSmall} className='w-9 h-10' resizeMode='contain' />
+            </View>
+          </View>
+          <SearchInput />
+          <View className='w-full flex-1 pt-5 pb-8'>
+            <Text className='text-gray-100 text-lg mb-3 font-pregular'>Latest Videos</Text>
+            <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />
+          </View>
+        </View>
+      )} ListEmptyComponent={() => (
+        <EmptyState title='No Videos Found' subtitle='Be the first one to upload a video.' />
+      )} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />} />
+    </SafeAreaView>
   );
 };
 
