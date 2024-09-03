0cdc466949efac647a70a0551e7e1fe1767e0384
Basel <www.basel10001@gmail.com>
2024-09-04 02:35:21 +0300

    pass query & redirect to its corresponding page.

--- app/search/[query].tsx
+++ app/search/[query].tsx
@@ -1,11 +1,15 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
+
   return (
-    <View>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <Text className='text-white text-xl'>{query}</Text>
+    </SafeAreaView>
   );
 };
 
--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,18 +1,30 @@
-import { View, TouchableOpacity, Image } from 'react-native';
+import { View, TouchableOpacity, Image, Alert } from 'react-native';
 import React, { useState } from 'react';
 import { TextInput } from 'react-native';
 import { icons } from '../constants';
+import { router, usePathname } from 'expo-router';
 
-const SearchInput = ({ value, handleChange }: any) => {
-    const [showPassword, setshowPassword] = useState(false);
+const SearchInput = () => {
+    const pathname = usePathname();
+    const [query, setQuery] = useState('');
 
     return (
         <View className='w-full h-16 relative'>
-            <TextInput value={value} placeholder={'Search for a video topic'} placeholderTextColor={'#7b7b8b'}
-                onChangeText={handleChange}
+            <TextInput value={query} placeholder={'Search for a video topic'}
+                placeholderTextColor={'#CDCDE0'} onChangeText={(e) => setQuery(e)}
                 className='w-full h-full px-4 mt-1 flex-1 bg-black-100 border-black-200 border-2 rounded-2xl items-center caret-secondary text-base font-pregular text-white focus:border-secondary space-x-4' />
             <TouchableOpacity className='absolute top-6 right-2'
-                onPress={() => setshowPassword(!showPassword)}>
+                onPress={() => {
+                    if (!query) {
+                        return Alert.alert('Missing Query', 'Please input something to search result accross database.');
+                    }
+
+                    if (pathname.startsWith('/search')) {
+                        router.setParams({ query });
+                    } else {
+                        router.push(`/search/${query}`);
+                    }
+                }}>
                 <Image source={icons.search}
                     resizeMode='contain' className='w-5 h-5' />
             </TouchableOpacity>
